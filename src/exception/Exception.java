a66e5a9840e4ed0be3fedeba862be98d2cf07578
LeeJiEun <gsilverg197@gmail.com>
2024-08-10 15:03:37 +0900

    🚀 Chore: add develop branch

---
+++ src/exception/Exception.java
@@ -0,0 +1,6 @@
+package exception;
+
+public class Exception  extends RuntimeException {
+
+}
+
---
+++ src/repository/repositoryImpl/RepositoryImpl.java
@@ -0,0 +1,7 @@
+package repository.repositoryImpl;
+
+import repository.Repository;
+
+public class RepositoryImpl implements Repository {
+
+}
