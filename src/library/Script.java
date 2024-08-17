6c7edc4d5e9b11b8ed2f99bd7f5cf8ceb1180bd7
YejinKimHanyang <yejinkim@hanyang.ac.kr>
2024-08-17 22:41:08 +0900

    merge

--- src/library/Script.java
+++ src/library/Script.java
@@ -1,38 +1,5 @@
 package library;
 
-<<<<<<< HEAD
-import common.Menu;
-
-public class Script {
-
-  StringBuilder script = new StringBuilder();
-
-  private void print(){
-    System.out.println(script);
-    script.delete(0, script.length());
-  }
-
-  public void selectMenu(){
-    script.append(Menu.STOCKREQUESTMENUSELECT.getDescription());
-    print();
-  }
-  public void readStockRequest(){
-    script.append(Menu.FINDCONDITION.getDescription());
-    print();
-  }
-  public void readStockRequestStatus(){
-    script.append(Menu.FINDSTATUSWMS.getDescription());
-    print();
-  }
-
-  public void updateStockRequest(){
-    script.append(Menu.UPDATESTATUS.getDescription());
-    print();
-  }
-  public void cancelStockRequest(){
-  }
-
-=======
 import common.Member;
 import common.Menu;
 import common.ResponseMessage;
@@ -432,5 +399,24 @@ public class Script {
             .append(Member.ID.getDescription()).append(response).append("\n");
         print();
     }
->>>>>>> origin
+
+    public void selectMenu(){
+        script.append(Menu.STOCKREQUESTMENUSELECT.getDescription());
+        print();
+    }
+    public void readStockRequest(){
+        script.append(Menu.FINDCONDITION.getDescription());
+        print();
+    }
+    public void readStockRequestStatus(){
+        script.append(Menu.FINDSTATUSWMS.getDescription());
+        print();
+    }
+
+    public void updateStockRequest(){
+        script.append(Menu.UPDATESTATUS.getDescription());
+        print();
+    }
+    public void cancelStockRequest(){
+    }
 }
